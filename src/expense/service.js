14b79915245c82ff574c924ce6d9944685ad1c44
htet-waiyan <h.waeyan@gmail.com>
2019-12-10 21:25:25 +0800

    feat: add category document inside mtd transaction list

--- src/expense/service.js
+++ src/expense/service.js
@@ -29,7 +29,7 @@ export default class ExpenseService {
         { timestamp: { $gte: fromDate } },
         { timestamp: { $lte: toDate } },
       ],
-    });
+    }).populate('category');
   }
 
   /**
