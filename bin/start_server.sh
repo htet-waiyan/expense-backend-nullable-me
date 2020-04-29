80299091bcf351e2a772456482cb49e12aca3ad5
htet-waiyan <h.waeyan@gmail.com>
2020-04-29 22:33:38 +0800

    add stop server and change appspec.yml

---
+++ bin/start_server.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /home/ubuntu/apps/spllit-backend
+pm2 startOrRestart ecosystem.backend.json
--- bin/start_servers.sh
+++
@@ -1,8 +0,0 @@
- _#!/bin/bash_
-
-export PM2_HOME=/home/ubuntu/.pm2  
-pm2 delete all
-cd /home/ubuntu/apps/spllit-as
-pm2 start ecosystem.as.json
-cd /home/ubuntu/apps/spllit-backend
-pm2 start ecosystem.backend.json
