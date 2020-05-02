6221d823ec95248f83063e5985b3b75187aaa872
htet-waiyan <h.waeyan@gmail.com>
2020-05-02 22:01:37 +0800

    export nvm in start/stop shell script

--- bin/start_server.sh
+++ bin/start_server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 cd /home/ubuntu/apps/spllit-backend
 pm2 startOrRestart ecosystem.backend.json
